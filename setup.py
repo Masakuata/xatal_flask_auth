4557522d4cf3736650ffcc6fe12b844ca2bb5ae7
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2022-10-14 22:48:01 -0500

    Se ha actualizado la lista de paquetes necesarios

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup
 
 setup(
 	name='xf_auth',
-	version='0.6.1',
+	version='0.6.2',
 	description='A package to handle auth on flask. Made by Xatal',
 	url='https://github.com/Masakuata/xatal_flask_auth',
 	author='Edson Manuel Carballo Vera',
 	author_email='edsonmanuelcarballovera@gmail.com',
 	license='MIT License',
 	packages=['xf_auth'],
-	install_requires=['cryptography==3.1.1', 'Flask'],
+	install_requires=[
+		'cryptography==3.1.1',
+		"Flask~=2.2.2",
+		"Werkzeug~=2.2.2",
+		"requests~=2.28.1",
+		"setuptools~=60.2.0",
+	],
 
 	classifiers=[
 		'Development Status :: 2 - Pre-Alpha',
